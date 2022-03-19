a5cafc63237f588aa53cca9c7ff869e740d54914
Tina Gao <tina.hm.gao@gmail.com>
2022-03-19 17:03:39 +1300

    change to atlas and add nginx

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://mongo:27017/auckland_property', { useNewUrlParser: true }).catch(e => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).catch(e => {
   console.error('Connection error', e.message);
 });
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const apiPort = 8000;
+require('dotenv').config();
 
 const db = require('./db');
 const propertyRouter = require('./routes/property-router');
