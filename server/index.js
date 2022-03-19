f0f53d28381ef909b1c182d450ce0f985d8e56aa
Tina Gao <tina.hm.gao@gmail.com>
2022-03-20 09:55:17 +1300

    local containerize with nginx

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const apiPort = 8000;
 require('dotenv').config();
+
+const apiPort = process.env.PORT || 4000;
 
 const db = require('./db');
 const propertyRouter = require('./routes/property-router');
